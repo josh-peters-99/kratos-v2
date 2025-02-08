c7632114af3940dba2f069eb0694bd9d83235898
JoshuaPeters <jgp2.11.99@gmail.com>
2025-02-08 14:47:23 -0700

    changed User import again

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectDB } from "@/lib/mongodb";
-import User from "../../../../models/User";
+import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export const authOptions = {
